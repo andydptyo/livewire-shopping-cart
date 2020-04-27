b3dffdc4161debec451e984cecb923b9be57e119
andydptyo <andy.dptyo@gmail.com>
2020-04-27 16:29:14 +0700

    initial commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->text(100),
+        'price' => $faker->numberBetween(1000, 10000)
+    ];
+});
